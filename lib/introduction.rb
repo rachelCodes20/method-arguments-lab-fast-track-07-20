65231ca3bee4d814d2d98efdfde23f21de5eff0d
Rachel Collins <rachel.collins@my.liu.edu>
2020-07-21 16:59:03 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,17 @@
+
+def introduction(name)
+  
+  puts "Hi, my name is #{name}."
+  
+end
+name = "Dan"
+
+introduction("Dan")
+
+def introduction_with_language (name, language)
+  
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  
+end
+language = "Ember.js"
+introduction_with_language("Dan", "Ember.js")
